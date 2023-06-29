5d9cb87a22e961e7be564535bdb063af96906276
Ishan Azad <ishansazad@gmail.com>
2023-06-28 17:53:15 -0700

    Set random square to red when clicking start

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 $(document).ready(function () {
-	console.log("I'm Ready");
+	const $startButton = $("#start");
+	const $squares = $(".square");
+
+	$startButton.on("click", function () {
+		randomSquare();
+	});
+	function randomSquare() {
+		const randomSquare = $squares[Math.floor(Math.random() * $squares.length)];
+		$(randomSquare).addClass("red-bg");
+
+		setTimeout(function () {
+			$(randomSquare).removeClass("red-bg");
+		}, 2000);
+	}
 });
